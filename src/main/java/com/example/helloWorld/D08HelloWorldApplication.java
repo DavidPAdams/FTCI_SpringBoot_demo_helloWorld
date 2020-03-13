78919e56ff0f943d7d611e1de02933246447bfbe
David Adams <david.riding.rails@gmail.com>
2020-03-13 11:41:08 -0700

    add input from user to project main method statements

--- src/main/java/com/example/helloWorld/D08HelloWorldApplication.java
+++ src/main/java/com/example/helloWorld/D08HelloWorldApplication.java
@@ -1,5 +1,7 @@
 package com.example.helloWorld;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +10,17 @@ public class D08HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D08HelloWorldApplication.class, args);
+		Scanner input = new Scanner(System.in);
 //		System.out.println("Hello Java World!!");
 //		System.out.println("Today is a great day for Java in Spring Boot");
-		String firstName = "Zack";
+//		String firstName = "Zack";
+//		System.out.printf("Hello %s!\n", firstName);
+//		System.out.printf("Goodbye %s", firstName);
+		System.out.println("What is your name?");
+		String firstName = input.nextLine();
+		input.close();
 		System.out.printf("Hello %s!\n", firstName);
-		System.out.printf("Goodbye %s", firstName);
+		System.out.printf("\tGoodbye %s\n", firstName);
 	}
 
 }
