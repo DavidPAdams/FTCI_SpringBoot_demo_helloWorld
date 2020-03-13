65af3cee93cf2132e01931d9333be139e6d302f7
David Adams <david.riding.rails@gmail.com>
2020-03-13 11:12:02 -0700

    initial commit

---
+++ src/main/java/com/example/helloWorld/D08HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D08HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D08HelloWorldApplication.class, args);
+	}
+
+}
