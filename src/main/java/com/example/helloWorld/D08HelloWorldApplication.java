2d8a360e3c1f1a7cadbc87bcc69548fa4ad426ac
David Adams <david.riding.rails@gmail.com>
2020-03-13 11:33:07 -0700

    change project main method statements with variable

--- src/main/java/com/example/helloWorld/D08HelloWorldApplication.java
+++ src/main/java/com/example/helloWorld/D08HelloWorldApplication.java
@@ -8,8 +8,12 @@ public class D08HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D08HelloWorldApplication.class, args);
-		System.out.println("Hello Java World!!");
-		System.out.println("Today is a great day for Java in Spring Boot");
+//		System.out.println("Hello Java World!!");
+//		System.out.println("Today is a great day for Java in Spring Boot");
+		String firstName = "Zack";
+		System.out.printf("Hello %s!\n", firstName);
+		System.out.printf("Goodbye %s", firstName);
 	}
 
 }
+
