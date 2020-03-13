d21a3fc92b7eb3e6d818ec5ee43a1598d33cf502
David Adams <david.riding.rails@gmail.com>
2020-03-13 11:52:37 -0700

    reset statements in project main method as comments

--- src/main/java/com/example/helloWorld/D08HelloWorldApplication.java
+++ src/main/java/com/example/helloWorld/D08HelloWorldApplication.java
@@ -10,25 +10,29 @@ public class D08HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D08HelloWorldApplication.class, args);
-//		Scanner input = new Scanner(System.in);
-//		System.out.println("Hello Java World!!");
+		System.out.println("Hello Java World!!");
+		
 //		System.out.println("Today is a great day for Java in Spring Boot");
+
 //		String firstName = "Zack";
 //		System.out.printf("Hello %s!\n", firstName);
 //		System.out.printf("Goodbye %s", firstName);
+		
+//    Scanner input = new Scanner(System.in);
 //		System.out.println("What is your name?");
 //		String firstName = input.nextLine();
 //		input.close();
 //		System.out.printf("Hello %s!\n", firstName);
 //		System.out.printf("\tGoodbye %s\n", firstName);
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("What is your first name?");
-		String firstName = userInput.nextLine();
-		System.out.println("What is your last name?");
-		String lastName = userInput.nextLine();
-		userInput.close();
-		System.out.printf("Hello %s %s!\n", firstName, lastName);
-		System.out.printf("\t\tGoodbye %s %s", firstName, lastName);
+		
+//		Scanner userInput = new Scanner(System.in);
+//		System.out.println("What is your first name?");
+//		String firstName = userInput.nextLine();
+//		System.out.println("What is your last name?");
+//		String lastName = userInput.nextLine();
+//		userInput.close();
+//		System.out.printf("Hello %s %s!\n", firstName, lastName);
+//		System.out.printf("\t\tGoodbye %s %s", firstName, lastName);
 	}
 
 }
