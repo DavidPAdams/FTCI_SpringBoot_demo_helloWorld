f08eec067f9098a8d1964a7958d0287eacc5bd3a
David Adams <david.riding.rails@gmail.com>
2020-06-12 13:09:13 -0700

    more adjustments for naming consistency

--- src/test/java/com/example/helloWorld/DemoHelloWorldApplicationTests.java
+++ src/test/java/com/example/helloWorld/DemoHelloWorldApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class D08HelloWorldApplicationTests {
+class DemoHelloWorldApplicationTests {
 
 	@Test
 	void contextLoads() {
